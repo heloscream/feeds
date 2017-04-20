00d311fb983ed708f79d2e9c1800d25cb4dd8f9c
heloscream <hemant@kreatio.com>
2017-04-20 19:14:28 +0530

    testing folder added

---
+++ src/app/example/example.service.ts
@@ -0,0 +1,18 @@
+// import { Injectable } from "@angular/core";
+// @Injectable()
+// export class ExampleService{
+//    someMethod(){
+//      return 'Hey!';
+//    }
+// }
+
+import { Injectable } from "@angular/core";
+@Injectable()
+export class ExampleService{
+   someMethod(){
+      return 'second method';
+   }
+   someMethod2(){
+     return 'second method';
+   }
+}
