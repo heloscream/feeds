4116f64aaf2d3bebb0c2b0a7f83f8c6f37021e75
heloscream <hemant@kreatio.com>
2017-04-27 15:50:52 +0530

    navigator changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component ,OnInit } from '@angular/core';
+import 'rxjs/add/operator/filter';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+   currentNav:string;
+    constructor(
+      private route: ActivatedRoute,
+      private router: Router
+    ) { }
+    ngOnInit() {
+    this.router.events
+      .filter(event => event instanceof NavigationEnd)
+      .subscribe(event => {
+        let currentRoute = this.route.root;
+        while (currentRoute.children[0] !== undefined) {
+          currentRoute = currentRoute.children[0];
+        }
+        this.currentNav =currentRoute.snapshot.data['name'];
+      })
   }
 
 }
+
+// import { Component ,OnInit } from '@angular/core';
+// import 'rxjs/add/operator/filter';
+// import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+//
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent implements OnInit {
+//   constructor(
+//     private route: ActivatedRoute,
+//     private router: Router
+//   ) { }
+//   ngOnInit() {
+//   this.router.events
+//     .filter(event => event instanceof NavigationEnd)
+//     .subscribe(event => {
+//       let currentRoute = this.route.root;
+//       while (currentRoute.children[0] !== undefined) {
+//         currentRoute = currentRoute.children[0];
+//       }
+//       currentRoute.snapshot.data['name'];
+//     })
+// }
+//
+//
+// }
--- src/app/left-nav/left-nav.component.ts
+++ src/app/left-nav/left-nav.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router,ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-left-nav',
   templateUrl: './left-nav.component.html',
   styleUrls: ['./left-nav.component.css']
 })
 export class LeftNavComponent implements OnInit {
-
-  constructor() { }
+  constructor(router:ActivatedRoute ) {
+   console.log(router.routeConfig);
+  }
 
   ngOnInit() {
   }
